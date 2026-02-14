6740fbb57f86d8bdf5430531ab8fd624ff5dc4d6
Debaprakash Jena <debaprakashjena2021@gmail.com>
2026-02-14 18:32:05 +0530

    server with socket handler
    Signed-off-by: Debaprakash Jena <debaprakashjena2021@gmail.com>

--- server.js
+++ server.js
@@ -1,13 +1,30 @@
+import  "dotenv/config"
 import dotenv from "dotenv";
 dotenv.config();
-
 import app from "./app.js";
+import http from "http";
 import connectDB from "./config/db.js";
 
+import { Server } from "socket.io";
+import { registerSocketHandlers } from "./sockets/socket.js";
+
 const PORT = process.env.PORT || 8080;
+ 
 
 connectDB();
 
-app.listen(PORT, () => {
+
+const server = http.createServer(app);
+const io = new Server(server, {
+  cors: {
+    origin: "*",
+    credentials: true
+  }
+});
+
+//Register socket logic
+registerSocketHandlers(io);
+
+server.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
